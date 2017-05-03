0479ad9a62ef0e1927b36899d11f591b5cc93299
salwa <salwaoudad@gmail.com>
2017-05-03 16:59:50 +0200

    Fin Footer

--- wordpress/wp-content/themes/mh-magazine-lite-child/footer.php
+++ wordpress/wp-content/themes/mh-magazine-lite-child/footer.php
@@ -34,11 +34,6 @@
 		</div>
 	</footer>
 <?php } ?>
-<div class="mh-copyright-wrap">
-	<div class="mh-container mh-container-inner clearfix">
-		<p class="mh-copyright"><?php printf(esc_html__('Copyright &copy; %1$s | WordPress Theme by %2$s', 'mh-magazine-lite'), date("Y"), '<a href="' . esc_url('https://www.mhthemes.com/') . '" rel="nofollow">MH Themes</a>'); ?></p>
-	</div>
-</div>
 <?php mh_after_footer(); ?>
 <?php wp_footer(); ?>
 </body>
