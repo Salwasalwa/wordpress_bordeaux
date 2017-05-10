c21f580834653c28a176d6ffa1683dc96cbffdfb
karina BRANTE <karina.brante@gmail.com>
2017-05-10 16:39:01 +0200

    add slider

--- wordpress/wp-content/themes/mh-magazine-lite-child/footer.php
+++ wordpress/wp-content/themes/mh-magazine-lite-child/footer.php
@@ -1,40 +1,44 @@
 <?php mh_before_footer(); ?>
 <?php if (is_active_sidebar('footer-1') || is_active_sidebar('footer-2') || is_active_sidebar('footer-3') || is_active_sidebar('footer-4')) { ?>
-	<footer class="mh-footer" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
-		<div class="mh-container mh-container-inner mh-footer-widgets mh-row clearfix">
-			<?php if (is_active_sidebar('footer-1')) { ?>
-				<div class="mh-col-1-4 mh-widget-col-1 mh-footer-area mh-footer-1">
-					<?php dynamic_sidebar('footer-1'); ?>
-				</div>
-			<?php } ?>
-			<?php if (is_active_sidebar('footer-2')) { ?>
-				<div class="mh-col-1-4 mh-widget-col-1 mh-footer-area mh-footer-2">
-					<?php dynamic_sidebar('footer-2'); ?>
-				</div>
-			<?php } ?>
-			<?php if (is_active_sidebar('footer-3')) { ?>
-				<div class="mh-col-1-4 mh-widget-col-1 mh-footer-area mh-footer-3">
-					<?php dynamic_sidebar('footer-3'); ?>
-				</div>
-			<?php } ?>
-			<?php if (is_active_sidebar('footer-4')) { ?>
-				<div class="mh-col-1-4 mh-widget-col-1 mh-footer-area mh-footer-4">
-					<p>Mairie de Bordeaux</p>
-					<p>Place Pey Berland</p>
-					<p>33000 Bordeaux</p>
-					<p>Adresse postale : </p>
-					<p>Place Pey-Berland</p>
-					<p>33045 Bordeaux Cedex</p></br>
-					<p>Contact</p>
-					<p>Du lundi au vendredi de 8h30 a 18h</p>
-					<p>Tél: 05 56 10 20 30</p></br>
-					<?php dynamic_sidebar('footer-4'); ?>
-				</div>
-			<?php } ?>
-		</div>
-	</footer>
-<?php } ?>
-<?php mh_after_footer(); ?>
-<?php wp_footer(); ?>
-</body>
-</html>
+    <footer class="mh-footer" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
+        <div class="slider">
+            <?php echo do_shortcode("[huge_it_slider id='1']"); ?>
+        </div>
+
+        <div class="mh-container mh-container-inner mh-footer-widgets mh-row clearfix">
+            <?php if (is_active_sidebar('footer-1')) { ?>
+                <div class="mh-col-1-4 mh-widget-col-1 mh-footer-area mh-footer-1">
+                    <?php dynamic_sidebar('footer-1'); ?>
+                </div>
+                <?php } ?>
+                <?php if (is_active_sidebar('footer-2')) { ?>
+                    <div class="mh-col-1-4 mh-widget-col-1 mh-footer-area mh-footer-2">
+                        <?php dynamic_sidebar('footer-2'); ?>
+                    </div>
+                    <?php } ?>
+                    <?php if (is_active_sidebar('footer-3')) { ?>
+                        <div class="mh-col-1-4 mh-widget-col-1 mh-footer-area mh-footer-3">
+                            <?php dynamic_sidebar('footer-3'); ?>
+                        </div>
+                        <?php } ?>
+                        <?php if (is_active_sidebar('footer-4')) { ?>
+                            <div class="mh-col-1-4 mh-widget-col-1 mh-footer-area mh-footer-4">
+                                <p>Mairie de Bordeaux</p>
+                                <p>Place Pey Berland</p>
+                                <p>33000 Bordeaux</p>
+                                <p>Adresse postale : </p>
+                                <p>Place Pey-Berland</p>
+                                <p>33045 Bordeaux Cedex</p></br>
+                                <p>Contact</p>
+                                <p>Du lundi au vendredi de 8h30 a 18h</p>
+                                <p>Tél: 05 56 10 20 30</p></br>
+                                <?php dynamic_sidebar('footer-4'); ?>
+                            </div>
+                            <?php } ?>
+                        </div>
+                    </footer>
+                    <?php } ?>
+                    <?php mh_after_footer(); ?>
+                    <?php wp_footer(); ?>
+                </body>
+                </html>
